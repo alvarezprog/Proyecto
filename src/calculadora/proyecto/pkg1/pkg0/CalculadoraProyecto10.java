e0ddad7609a07ba1925a4745199bb0b443a727d6
20PROGB18 <20PROGB18@TS-LABS-2.labs.cbtis94.local>
2021-11-18 09:59:32 -0600

    Primer versiòn vacia

---
+++ src/calculadora/proyecto/pkg1/pkg0/CalculadoraProyecto10.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora.proyecto.pkg1.pkg0;
+
+/**
+ *
+ * @author 20PROGB18
+ */
+public class CalculadoraProyecto10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
